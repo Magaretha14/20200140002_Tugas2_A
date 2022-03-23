be8d3a3ad95c8016da00438cbda4fa1ecebab120
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-03-24 00:27:06 +0700

    Create Tabel transaksi ('gk sengaja ketiduran jdinya lama :)')

--- 20200140002_Tugas2_A/Program.cs
+++ 20200140002_Tugas2_A/Program.cs
@@ -18,7 +18,8 @@ namespace _20200140002_Tugas2_A
                 con.Open();
 
                 SqlCommand cm = new SqlCommand("create table Pemilik (NIK_Pemilik char(7) not null primary key, Nama_Pemilik varchar(50) not null, No_Hp char(12), Alamat varchar(50))"
-                    + "create table Penyewa (NIK_Penyewa char(7) not null primary key, Nama_Penyewa varchar(50) not null, No_Hp char(12), Alamat varchar(50))", con);
+                    + "create table Penyewa (NIK_Penyewa char(7) not null primary key, Nama_Penyewa varchar(50) not null, No_Hp char(12), Alamat varchar(50))"
+                    + "create table Transaksi (Id_Transaksi char(5) not null primary key, NIK_Pemilik char(7) FOREIGN KEY REFERENCES Pemilik(NIK_Pemilik), NIK_Penyewa char(7) FOREIGN KEY REFERENCES Penyewa(NIK_Penyewa), Waktu varchar(20), TempoKontrak varchar(50), Alamat_Toko varchar(50), Harga varchar(50))", con);
                 cm.ExecuteNonQuery();
 
                 Console.WriteLine("Tabel sukses dibuat!");
