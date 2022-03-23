790bb012b5ecb8b482bc764c2c47e0ed7aa5a850
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-03-23 22:57:44 +0700

    Add project files.

---
+++ 20200140002_Tugas2_A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace _20200140002_Tugas2_A
+{
+    class Program
+    {
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection("data source=LAPTOP-N3UNMQ7P;database=Puput;Integrated Security = TRUE");
+                con.Open();
+
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
